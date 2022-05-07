91ac13e3111328393fc2f3be79f472dac3270bb8
leecu <leecu@DESKTOP-0L4I8UV>
2022-05-08 06:39:11 +0900

    작업내용 : 로그인 버튼 눌를시 프로시저 실행되면서 신규 계정일경우 insert되도록 작업 이제 에러가 나오지 않음

--- Tetrise/src/MainWindow.java
+++ Tetrise/src/MainWindow.java
@@ -1,39 +1,27 @@
-import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-
-import java.sql.*;
-import java.sql.SQLException;
 
 public class MainWindow extends JFrame{
 	
 	static Connection conn = null;
 	static Statement stmt = null;
 	static ResultSet rs = null;
-	
-	public static void connectDB() throws ClassNotFoundException{
-		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			String connectionUrl="jdbc:sqlserver://localhost:14033;database=Tetrise;user=smlee;password=cowgolem02*;encrypt=true;trustServerCertificate=true";
-			
-			conn = DriverManager.getConnection(connectionUrl);
-			stmt = conn.createStatement();
-			System.out.println("MS-SQL 서버 연결에 성공하였습니다.");
-			
-			stmt.close();
-			conn.close();
-		}
-		catch(SQLException sqle){
-			System.out.println("SQLException : " + sqle);
-		}
-	}
+		
+	String userGrade = null;
+	String userClass = null;
+	String userNumber = null;
+	String userName = null;
+	String rst  = null;
 	
 	public MainWindow(){
 		setTitle("Tetrise");
@@ -97,39 +85,54 @@ public class MainWindow extends JFrame{
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
 				
-				String userGrade = edUserGrade.getText();
-				String userClass = edUserClass.getText();
-				String userNumber = edUserNumber.getText();
-				String userName = edUserName.getText();
+				userGrade = edUserGrade.getText();
+				userClass = edUserClass.getText();
+				userNumber = edUserNumber.getText();
+				userName = edUserName.getText();
 				
 				
 				try {
-					login(userGrade, userClass, userNumber, userName);
+					rst = login(userGrade, userClass, userNumber, userName);
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
+								
 			}
 		});
 		
 		setVisible(true);
 	}
 	
+	protected String login(String userGrade, String userClass, String userNumber, String userName) throws SQLException {
+		// TODO Auto-generated method stub
+		rs = stmt.executeQuery("exec member_sign_in @user_grade=N'" + userGrade + "', @user_class=N'" + userClass + "', @user_number=N'" + userNumber + "', @user_name='" + userName + "'");
+		rs.next();
+		String result = rs.getString("result");
+				
+		return result;
+	}
 	
 	public static void main(String[] args) throws ClassNotFoundException {
 		// TODO Auto-generated method stub
-		new MainWindow();
-		connectDB();
+		
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			String connectionUrl="jdbc:sqlserver://localhost:14033;database=Tetrise;user=smlee;password=cowgolem02*;encrypt=true;trustServerCertificate=true";
+			
+			conn = DriverManager.getConnection(connectionUrl);
+			stmt = conn.createStatement();
+			System.out.println("MS-SQL서버 연결에 성공");
+			
+			new MainWindow();
+						
+			
+		}
+		catch(SQLException sqle) {
+			System.out.println("SQLException : " + sqle);
+		}
+		
 	}
 		
-	public void login(String userGrade, String userClass, String userNumber, String userName) throws SQLException {
-		rs = stmt.executeQuery("exec member_get");
-		//rs = stmt.executeQuery("exec member_sign_in @user_grade=N'" + userGrade + "', @user_class=N'" + userClass + "', @user_number=N'" + userNumber + "', @user_name='" + userName + "'");
-		rs.next();
-		String result = rs.getString("member_id");
-		System.out.println(result);
-		
-	}
 
 }
